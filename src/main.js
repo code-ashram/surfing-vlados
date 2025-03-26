7bc899e4c3abe26046bbc793f8f143ae397a40de
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-03-26 13:27:46 +0300

    add swiper for header

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './style.scss';
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import './style.scss';
 
 const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
     loop: true,
     slidesPerView: 1,
     navigation: {
